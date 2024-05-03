9df2fd4dccef1587bc368d68ba93489be3d771dd
Wendell Pires <132901688+wpsdigital@users.noreply.github.com>
2024-05-03 16:05:48 -0300

    Update index.js

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,9 +1,20 @@
+//  icons
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from 'react-icons/rx'
+
+// components
 import ServiceSlider from '../../components/ServiceSlider';
 import Bulb from '../../components/Bulb';
 import Circles from '../../components/Circles';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../../variants';
 
+//framer motion
+import { motion } from 'framer-motion';
+import {fadeIn} from '../../variants';
 
 const Services = () => {
   return (
@@ -11,38 +22,19 @@ const Services = () => {
       <Circles />
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
-          {/** Text */}
+          {/* text */}
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <motion.h2 
-              variants={fadeIn('up', 0.3)}
-              initial='hidden'
-              animate='show'
-              exit='exit'
-              className='h2 xl:mt-8'>
-              My service <span className='text-accent'>.</span>
-            </motion.h2>
-            <motion.p 
-              variants={fadeIn('up', 0.4)}
-              initial='hidden'
-              animate='show'
-              exit='exit'
-              className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-              I`m here to save your time and help you do better with the time you earn.I am ready to help you with everything from A to Z in your new projects.
-            </motion.p>
-          </div>
-          <motion.div 
-            variants={fadeIn('down', 0.5)}
-            initial='hidden'
-            animate='show'
-            exit='exit'
-            className='w-full xl:max-w-[65%]'>
-            {/** Slider */}
-          <ServiceSlider />
-          </motion.div>
+            <motion.h2 variants={fadeIn('up', 0.2)} initial='hidden' animate='show' exit='hidden' className='h2 xl:mt-8'>Meus Serviços <span className='text-accent'>.</span></motion.h2>
+            <motion.p variants={fadeIn('up', 0.4)} initial='hidden' animate='show' exit='hidden' className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Esses são apenas alguns dos serviços que eu posso oferecer, e muitos deles podem se sobrepor ou serem combinados para atender às necessidades específicas de cada cliente ou empresa.</motion.p>
         </div>
+        {/* slider */}
+        <motion.div variants={fadeIn('down', 0.6)} initial='hidden' animate='show' exit='hidden' className='w-full xl:max-w-[65%]'>
+          <ServiceSlider />
+        </motion.div>        
       </div>
-      <Bulb />
     </div>
+    <Bulb />
+  </div>
   );
 };
 
